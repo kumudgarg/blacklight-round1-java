2357b6ae75cf966b466144241182bf514ccf619c
Kumud <kumud50garg@gmail.com>
2020-11-04 16:59:23 +0530

    Welcome to user-game interaction

---
+++ src/main/java/com/blacklight/KumudRound1java/KumudRound1JavaApplication.java
@@ -0,0 +1,13 @@
+package com.blacklight.KumudRound1java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumudRound1JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumudRound1JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blacklight/KumudRound1java/KumudRound1JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacklight.KumudRound1java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumudRound1JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
